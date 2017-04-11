b73e6c7003139e5ccc0c32186f59642572aef1f9
Antonio Morales <amorales@cs.hartford.edu>
2017-04-11 17:50:46 -0400

    Created constructor for Translate Class

--- Tabula/Tabula/TranslateMedia.cs
+++ Tabula/Tabula/TranslateMedia.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Tabula
 {
     class TranslateMedia
     {
+        Rectangle ImageSelected;
+
+
+        public TranslateMedia(Rectangle ImageSelected)
+        {
+            this.ImageSelected = ImageSelected;
+
+        }
+
     }
 }

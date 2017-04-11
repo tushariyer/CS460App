0ad74072adfc071bd189bb0b119484ad79404d05
brian tata <brian tata>
2017-04-10 23:29:52 -0400

    Added pseudocode to meme.cs for planning purposes

--- Tabula/Tabula/Meme.cs
+++ Tabula/Tabula/Meme.cs
@@ -4,19 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Tabula
 {
     class Meme
     {
         private TextBox topText, bottomText;
+        private string userInput1,userInput2;
 
-        public Meme()
+        //Takes in image from the main form and sets up meme(TM) text
+        public Meme(Image image)
         {
+            //Check to see if the user wants default meme text or nah
 
+            //Get text from user if needed
+            userInput1 = TextInput.TextDialog("Top Text", "Enter your text");
+            userInput2 = TextInput.TextDialog("Bottom Text", "Enter your text");
+
+            //Set text boxes to have text from user/default 
+            //Also formatting text box size,position,etc. here
+
+            //Returns(?) image to be placed on canvas afterward
         }
 
+        //Method to handle setting default text
 
 
+        //Method to handle setting user text
+
+
+        //Method to handle formatting
+
+
+        //Method to render image without editable text boxes
     }
 }

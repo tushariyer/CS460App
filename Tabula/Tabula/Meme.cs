c71d29736bf052c70d7a8c2c09f404613c8bd6b3
brian tata <brian tata>
2017-04-10 22:40:20 -0400

    Started Meme.cs.  Added a class to make text input pop ups(might not be working need to test) so that you guys can use it if needed

---
+++ Tabula/Tabula/Meme.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabula
+{
+    class Meme
+    {
+        private TextBox topText, bottomText;
+
+        public Meme()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Tabula/Tabula/TextInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabula
+{
+    //Class used to pop up an input dialog box for text input
+    public static class TextInput
+    {
+        public static string TextDialog(string title, string defaultVal)
+        {
+            //Creates new form with these properties
+            Form popUp = new Form()
+            {
+                Width = 300,
+                Height = 100,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = defaultVal,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            //Sets up control buttons and sets their individual properties(height,width,etc.)
+            Label popupTitle = new Label() { Left = 20, Top = 10, Text = title};
+            TextBox textBox = new TextBox() {Left = 20, Top = 50, Width = 200 };
+            Button ok = new Button() {Text = "Enter", Left = 150, Top = 90, Width = 50, DialogResult = DialogResult.OK};
+
+            //Adds the buttons to the actual form
+            popUp.Controls.Add(popupTitle);
+            popUp.Controls.Add(textBox);
+            popUp.Controls.Add(ok);
+            popUp.AcceptButton = ok;
+
+            //Returns string hopefully
+            return textBox.Text;
+        }
+
+    }
+}

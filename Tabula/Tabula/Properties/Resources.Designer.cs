4a531b232dad70d15403a9418e4cb0d200bc1a2f
Tushar Iyer <Tushar Iyer>
2017-04-18 08:12:32 -0400

    Logo has been created, but I have absolutely no idea how to set it. So we've got that going for us....which is nice

--- Tabula/Tabula/Properties/Resources.Designer.cs
+++ Tabula/Tabula/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace Tabula.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Tabula {
+            get {
+                object obj = ResourceManager.GetObject("Tabula", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap WonkaMeme {

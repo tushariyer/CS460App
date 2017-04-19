1cbed31f1457940597d83a526b11d856a5e8e9b9
Tushar Iyer <Tushar Iyer>
2017-04-19 12:51:26 -0400

    Save is now protected...for real this time

--- Tabula/Tabula/SavePicture.cs
+++ Tabula/Tabula/SavePicture.cs
@@ -86,9 +86,9 @@ namespace Tabula
          */
         String CheckExtension(String sv)
         {
-            if (Path.HasExtension(sv) == false || Path.GetExtension(sv).ToLower() != ".png" || Path.GetExtension(sv).ToLower() != ".jpg" || Path.GetExtension(sv).ToLower() != ".jpeg" || Path.GetExtension(sv).ToLower() != ".bmp")
+            if (Path.GetExtension(sv).ToLower() != ".png" || Path.GetExtension(sv).ToLower() != ".jpg" || Path.GetExtension(sv).ToLower() != ".jpeg" || Path.GetExtension(sv).ToLower() != ".bmp")
             {
-                MessageBox.Show("Invalid extention.\nSaving as PNG");
+                Console.WriteLine("Invalid extention.\nSaving as PNG");
                 sv += ".png";
                 Console.WriteLine(sv);
             }

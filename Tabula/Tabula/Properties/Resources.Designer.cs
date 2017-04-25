ad6417456d873bb616035e949f933e098c0e2277
Brian M. Tata <btata@cs.hartford.edu>
2017-04-25 12:47:16 -0400

    Added icon to application

--- Tabula/Tabula/Properties/Resources.Designer.cs
+++ Tabula/Tabula/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Tabula.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon favicon {
+            get {
+                object obj = ResourceManager.GetObject("favicon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon Tabula {
             get {
                 object obj = ResourceManager.GetObject("Tabula", resourceCulture);
